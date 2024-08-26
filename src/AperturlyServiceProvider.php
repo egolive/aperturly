53afc011b1c5a9c404a3d70332cfcc22fe8a29f1
Daniel Ehrenhofer <daniel.ehrenhofer@jarltech.de>
2024-08-26 23:08:33 +0200

    update AperturlyInstallCommand

--- src/AperturlyServiceProvider.php
+++ src/AperturlyServiceProvider.php
@@ -59,58 +59,6 @@ class AperturlyServiceProvider extends PackageServiceProvider {
 
         $command->info('Aperturly installation complete.');
       });
-
-
-    $package
-      ->name('aperturly')
-      ->hasConfigFile() // Falls es eine Config-Datei gibt
-      ->hasMigrations([
-        'create_portfolios_table',
-        'create_galleries_table',
-        'create_series_table',
-        'create_images_table',
-      ])
-      ->hasInstallCommand(function (Command $command) {
-        $command
-          ->startWith(function (Command $command) {
-            $command->info('Starting Aperturly installation...');
-            if (!$command->confirm('Do you want to proceed with the installation?', true)) {
-              $command->warn('Installation aborted.');
-            }
-          })
-          ->publishMigrations()
-          ->askToRunMigrations()
-          ->publishAssets()
-          ->addInstallStep('Setting up routes', function (Command $command) {
-            $routesPath = base_path('routes/web.php');
-            $stubPath = __DIR__ . '/stubs/';
-
-            if ($command->confirm('Would you like to install Spatie Laravel-Permission for role management?', TRUE)) {
-              // Kopiere die Datei mit Spatie-Integration
-              file_put_contents($routesPath, file_get_contents($stubPath . 'web_with_permission.stub'), FILE_APPEND);
-              $command->call('aperturly:install-permission');
-            }
-            else {
-              // Kopiere die Datei ohne Spatie-Integration
-              file_put_contents($routesPath, file_get_contents($stubPath . 'web_without_permission.stub'), FILE_APPEND);
-            }
-
-            $command->info('Routes have been set up successfully.');
-          })
-          ->addInstallStep('Installing Breeze and compiling assets', function (Command $command) {
-            $command->info('Installing Breeze...');
-            $command->call('breeze:install', ['stack' => 'blade']);
-
-            $command->info('Installing npm dependencies...');
-            exec('npm install');
-
-            $command->info('Compiling assets...');
-            exec('npm run build');
-          })
-          ->endWith(function (Command $command) {
-            $command->info('Aperturly installation complete.');
-          });
-      });
   }
 
 }
